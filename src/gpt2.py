81c2e10ce6cc7921a1e1dc7e051a92a6874d4cb9
shirinyamani <amani.shirin@ucalgary.ca>
2024-09-30 14:02:11 -0600

    Block + GPTConfig added

--- src/gpt2.py
+++ src/gpt2.py
@@ -12,8 +12,19 @@ class GPTConfig:
     n_head = 6
     n_embed = 384
     
-class Block:
-    pass
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+            
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x 
+    
 class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
