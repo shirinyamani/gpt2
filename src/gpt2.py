2a739ff2141fbbbacaf1516db77cd8a4127acc4c
shirinyamani <amani.shirin@ucalgary.ca>
2024-10-02 11:10:06 -0600

    gpt2 loaded from hf checkpoints

--- src/gpt2.py
+++ src/gpt2.py
@@ -21,9 +21,8 @@ class CausalSelfAttention(nn.Module):
         
     def forward(self, x):
         B,T,C = x.size()
-        qkv = self.c_attn(x)
-        q,k,v = qkv.split(self.n_embed, dim=2)
-        
+        #qkv = self.c_attn(x)
+        q,k,v = self.c_attn(x).split(self.n_embed, dim=2)
         # (B,T,n_heads, C // n_head) => (B, n_heads, T, head_size)
         k = k.view(B,T, self.n_head, C // self.n_head).transpose(1,2)
         q = q.view(B,T, self.n_head, C // self.n_head).transpose(1,2)
@@ -42,13 +41,13 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
-        self.gelu = nn.GELU(approximate='tanh')
+        self.gelu = nn.GELU()
         self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
         
     def forwad(self, x):
-        x = self.c_fc
-        x = self.gelu
-        x = self.c_proj
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
         return x 
         
 class Block(nn.Module):
@@ -79,7 +78,7 @@ class GPT(nn.Module):
         
         self.transformer = nn.ModuleDict(dict( #index using str
             wte = nn.Embedding(config.vocab_size, config.n_embed),
-            wpe = nn.Embedding(config.vocab_size, config.n_embed),
+            wpe = nn.Embedding(config.block_size, config.n_embed),
             h = nn.ModuleList(list( #index usinh int
                 Block(config) for _ in range(config.n_layer)
                 )),
@@ -94,17 +93,18 @@ class GPT(nn.Module):
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
         from transformers import GPT2LMHeadModel
         print(f'loading weight from {model_type}')
-        
+
         config_args = {
-            'gpt2': dict(n_layer= 12, n_head=12, n_embed= 768),
-            'gpt2-medium': dict(n_layer= 24, n_head=16, n_embed= 1024),
-            'gpt2-large':dict(n_layer= 36, n_head=20, n_embed= 1280),
-            'gpt2-xl':dict(n_layer= 48, n_head=25, n_embed= 1600) 
+        'gpt2': dict(n_layer=12, n_head=12, n_embed=768),
+        'gpt2-medium': dict(n_layer=24, n_head=16, n_embed=1024),
+        'gpt2-large': dict(n_layer=36, n_head=20, n_embed=1280),
+        'gpt2-xl': dict(n_layer=48, n_head=25, n_embed=1600)
         }[model_type]
         
         config_args['vocab_size'] = 50257
         config_args['block_size'] = 1024 
-        
+        #config_args['bias'] = True 
+        #print("forcing vocab_size=50257, block_size=1024, bias=True")
         config = GPTConfig(**config_args)
         model = GPT(config)
         sd = model.state_dict()
@@ -120,6 +120,7 @@ class GPT(nn.Module):
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
         transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        assert sd_keys == sd_keys_hf
         # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
         # this means that we have to transpose these weights when we import them
         assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
@@ -130,11 +131,15 @@ class GPT(nn.Module):
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k].t())
             else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape, f'mismatched {sd_hf[k].shape} != {sd[k].shape}'
+                # print(f'shape hf model keys:{sd_hf[k].shape}, our model key shape is {sd[k].shape}')
+                # assert model.transformer.wte.weight.shape == model.lm_head.weight.shape, "Mismatch between wte and lm_head shapes."
+                # # vanilla copy over the other parameters
+                if sd_hf[k].shape != sd[k].shape:
+                    print(f"Problematic key: {k}")
+                assert sd_hf[k].shape  == sd[k].shape, f'mismatched {sd_hf[k].shape} != {sd[k].shape}'
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
         return model
 #=====================
 model = GPT.from_pretrained(model_type='gpt2')
-print('didnt crash!')
+print(f'Successfully loaded the weights from {model._get_name()}')
